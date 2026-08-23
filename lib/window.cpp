e3113160610999a81363311c3236e8c240102aba
Ron J <rojailal@gmail.com>
2026-08-22 20:12:57 -0700

    aurora: make PNG::PNG public on aurora_gx and define SDL_MAIN_HANDLED

--- lib/window.cpp
+++ lib/window.cpp
@@ -17,6 +17,7 @@
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_hints.h>
+#define SDL_MAIN_HANDLED
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_pixels.h>
